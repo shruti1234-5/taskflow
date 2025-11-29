7c986d40c64390fbd93d330f6df2de564a771e3f
Shruti <141910922+shruti1234-5@users.noreply.github.com>
2025-11-29 12:35:18 +0530

    Update page.jsx

--- src/app/(unauthenticated)/login/page.jsx
+++ src/app/(unauthenticated)/login/page.jsx
@@ -93,7 +93,7 @@ export default function LoginPage() {
             onChange={handleChange}
             required
             placeholder="Enter your password"
-            className="w-full px-4 py-2.5 border border-gray-300 dark:border-gray-700 rounded-lg bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100"
+            className="w-full px-4 py-2.5 border border-gray-300 dark:border-gray-700 dark:text-black rounded-lg bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100"
           />
         </div>
 
